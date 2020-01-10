5162c52a2436149ae42c8195e828010fd5f80e55
lubaoqing <1023165188@qq.com>
2020-01-10 21:01:14 +0800

    lll

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
+    <router-view></router-view>
+    <main-tabbar></main-tabbar>
   </div>
 </template>
 
 <script>
-
+  import MainTabbar from 'components/content/mainTabbar/MainTabbar'
   export default {
     name: 'app',
     components: {
+      MainTabbar
     }
   }
 </script>
---
+++ src/components/content/mainTabbar/MainTabbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <tabbar>
+      <tab-bar-item path="/home" activecolor="#ccc">
+        <img slot="item-icon" src="~assets/img/tabbar/home.png">
+        <img slot="item-icon-active" src="~assets/img/tabbar/home_active.png">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category">
+        <img slot="item-icon" src="~assets/img/tabbar/category.png">
+        <img slot="item-icon-active" src="~assets/img/tabbar/category_active.png">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart">
+        <img slot="item-icon" src="~assets/img/tabbar/shopcart.png">
+        <img slot="item-icon-active" src="~assets/img/tabbar/shopcart_active.png">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile">
+        <img slot="item-icon" src="~assets/img/tabbar/mine.png">
+        <img slot="item-icon-active" src="~assets/img/tabbar/mine_active.png">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import Tabbar from 'components/common/tabbar/TabBar'
+import TabBarItem from 'components/common/tabbar/TabBarItem'
+
+export default {
+  name: 'MainTabbar',
+  components: {
+    Tabbar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+
+</style>
