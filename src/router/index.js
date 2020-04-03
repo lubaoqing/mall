03785b78742381009b7ca892f68b01acad07a403
lubaoqing <1023165188@qq.com>
2020-04-03 10:54:49 +0800

    改回来

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
+const Home = () => import('views/home/Home')
+const Cart = () => import('views/cart/Cart')
+const Category = () => import('views/category/Category')
+const Profile = () => import('views/profile/Profile')
+const Detail = () => import('views/detail/Detail')
+
+// 安装插件
 Vue.use(VueRouter)
 
+// 创建路由对象
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/profile',
+    component: Profile
+  },
+  {
+    path: '/detail/:iid',
+    component: Detail
   }
 ]
-
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
+// 导出router
 export default router
